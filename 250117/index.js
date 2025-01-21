51f4539005e36b852c44f9fa825d8e85e6d58f01
JuDongGeon <ehdrjs981024@gmail.com>
2025-01-21 09:30:40 +0900

    250117 수업

---
+++ 250117/index.js
@@ -0,0 +1,17 @@
+console.log("Hi");
+
+// var let const 변수명 = 변수값
+// ex
+var apple = "apple";
+
+apple = "citrus";
+
+let Apple = "Apple";
+Apple = "MS";
+
+//hoisting 호이스팅 : 재선언
+//var와 let은 잘 사용하지 않음 대신 const를 사용
+
+const fruit = "blueberry";
+
+console.log(apple);
